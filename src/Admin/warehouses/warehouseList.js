03aa6cea158a983e9d4c5fb9c7a2ebc81d500c28
omarSamy212 <omarsamy0211@gmail.com>
2023-05-06 18:11:58 +0200

    connecting FE with BE in warehoue,supervisor components

--- src/Admin/warehouses/warehouseList.js
+++ src/Admin/warehouses/warehouseList.js
@@ -5,80 +5,104 @@ import "../header/header.css";
 import "./warehouseList.css";
 import Footer1 from "../footer/Footer1";
 import { Header } from "../header/header";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { getAuthToken } from "../../services/auth";
 export const WarehouseList = () => {
-  const warehouse = WarehouseDate;
-  const submit = (id) => {
-    console.log(`the id is: ${id}`);
-  };
 
-  return (
-    <div class="Omar3">
-      {/* <nav  id ="navv"class="navbar navbar-expand-lg navbar-dark bg-dark" margin-left='70px'>
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a  class="navbar-brand" href="#">
-      <img src="https://www.freeiconspng.com/thumbs/dashboard-icon/dashboard-icon-3.png" width="35" height="35" class="d-inline-block align-top" ></img>
-      </a>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          
-        <a class="navbar-brand" href="#">EDARA-DASHBOARED</a>
-        
-    
-          <li class="nav-item"> 
-          <Link to={"/addw"}class="nav-link active" aria-current="page" color="white" >Add Warehouse</Link>
-          </li>
+  const { token, user } = getAuthToken();
+  const [warehouses, setWarehouses] = useState({
+    loading: true,
+    result: [],
+    err: null,
+    update: false,
+  });
 
-          <li class="nav-item">
-            
-          <Link to={"/req"}class="nav-link active" aria-current="page" color="white" >Requests</Link>
-          </li>
-    
-  
-          <li class="nav-item">
-            
-          <Link to={"/SV"}class="nav-link active" aria-current="page" color="white" >Supervisors</Link>
-          </li>
-          
-        <li class="nav-item">
-          <Link to={"/"} class="nav-link active" aria-current="page" color="white"  >Logout</Link>
-          </li>
-  
-        </ul>
-        <form class="d-flex">
-         
-          
-        </form>
-      </div>
-    </div>
-  </nav> */}
-
-      <Header />
-      <Link to={"/addw"} style={{ backgroundColor: "#6096B4" }}>
-        <button type="button" className="btn btn-warning m-1">
-          Add Warehouse
-        </button>
-      </Link>
-
-      <div class="color">
-        <div class="d-flex flex-wrap justify-content-around container">
-          {warehouse.map((item) => {
+  const error = () => {
+    return (
+      <div className="container">
+        <div className="row">
+          {warehouses.err.map((err, index) => {
             return (
-              <Warehouses
-                key={item.id}
-                image={item.image}
-                id={item.id}
-                name={item.name}
-                location={item.location}
-                status={item.status}
-              />
+              <div
+                key={index}
+                className="col-sm-12 alert alert-danger"
+                role="alert"
+              >
+                {err.msg}
+              </div>
             );
           })}
         </div>
       </div>
-      <Footer1 />
+    );
+  };
+
+  useEffect(() => {
+    setWarehouses({ ...warehouses, loading: true });
+    axios
+      .get("http://localhost:4000/api/warehouse", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((data) => {
+        setWarehouses({
+          ...warehouses,
+          result: data.data,
+          loading: false,
+          err: null,
+        });
+      })
+      .catch((err) => {});
+  }, []);
+
+  const loadingSpinner = () => {
+    return (
+      <div className="container h-100">
+        <div className="row h-100 justify-content-center align-items-center">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div class="Omar3">
+      <Header />
+      {warehouses.err !== null && error()}
+      {warehouses.loading === true ? (
+        loadingSpinner()
+      ) : (
+        <>
+          <>
+            <Link to={"/addw"} style={{ backgroundColor: "#6096B4" }}>
+              <button type="button" className="btn btn-warning m-1">
+                Add Warehouse
+              </button>
+            </Link>
+            <div class="color">
+              <div class="d-flex flex-wrap justify-content-around container">
+                {warehouses.result.map((item) => {
+                  return (
+                    <Warehouses
+                      key={item.id}
+                      image={item.imageUrl}
+                      id={item.id}
+                      name={item.name}
+                      location={item.location}
+                      // status={item.status}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+            <Footer1 />
+          </>
+        </>
+      )}
     </div>
   );
 };
